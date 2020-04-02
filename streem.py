2f0b27628ab2a83f14846a864956ea7ed471f525
Georg <georg.groenendaal@gmail.com>
2020-04-02 16:00:38 +0200

    Initial commit

---
+++ streem.py
@@ -0,0 +1,87 @@
+import asyncio
+import pickle
+from typing import Callable, Dict, List, Tuple
+
+from redis.setup import create_redis
+
+from aioredis.errors import BusyGroupError
+import aioredis
+
+
+class StreemTask():
+    def __init__(self, streem_instance, stream_name, func):
+        self._streem_instance = streem_instance
+        self._stream_name = stream_name
+        self._func = func
+
+    def __call__(self, *args, **kwargs):
+        return self._func(*args, **kwargs)
+
+    async def send(self, *args, **kwargs):
+        await self._streem_instance._create_redis()
+
+        arguments = pickle.dumps(args)
+        kwarguments = pickle.dumps(kwargs)
+        data = {
+            "arguments": arguments,
+            "kwarguments": kwarguments
+        }
+
+        return await self._streem_instance._redis.xadd(self._stream_name, data)
+
+
+class Streem():
+    def __init__(self):
+        self._redis = None
+        self._callables: Dict[str, Callable] = {}
+
+    def task(self, stream_name: str):
+        def _decorator(func):
+            if stream_name in self._callables:
+                raise Exception(f"Stream: {stream_name} already registered")
+
+            self._callables[stream_name] = func
+            streem_task = StreemTask(self, stream_name, func)
+            return streem_task
+
+        return _decorator
+
+    async def start(self, consumer_name: str, group_name: str = "workers"):
+        await self._create_redis()
+        print(self._callables)
+
+        stream_names = list(self._callables.keys())
+        id_selectors = ["0"] * len(stream_names)
+
+        for stream_name in stream_names:
+            try:
+                await self._redis.xgroup_create(stream_name, group_name,
+                                                mkstream=True)
+            except BusyGroupError:
+                # print(f"Consumer group for '{stream_name}' already exists")
+                pass
+
+        while True:
+            messages = await self._redis.xread_group(group_name,
+                                                     consumer_name,
+                                                     stream_names,
+                                                     count=10,
+                                                     latest_ids=id_selectors)
+
+            for received_stream_name, message_id, data in messages:
+                received_stream_name = received_stream_name.decode("utf-8")
+                arguments = pickle.loads(data[b"arguments"])
+                kwarguments = pickle.loads(data[b"kwarguments"])
+
+                await self._callables[received_stream_name](*arguments,
+                                                            **kwarguments)
+
+                await self._redis.xack(received_stream_name, group_name,
+                                       message_id)
+
+            if not messages:
+                id_selectors = [">"] * len(stream_names)
+
+    async def _create_redis(self):
+        if not self._redis:
+            self._redis = await aioredis.create_redis_pool('redis://localhost')
